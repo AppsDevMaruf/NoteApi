97ae525b5997b51541a0789eeded9516e1d0a725
Maruf Alam <44240762+AppsDevMaruf@users.noreply.github.com>
2022-10-29 02:25:44 +0600

    add register ui design

---
+++ app/src/main/java/com/marufalam/noteappswithapi/NoteApplication.kt
@@ -0,0 +1,8 @@
+package com.marufalam.noteappswithapi
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NoteApplication:Application(){
+}
---
+++ app/src/main/java/com/marufalam/noteappswithapi/api/UserApi.kt
@@ -0,0 +1,15 @@
+package com.marufalam.noteappswithapi.api
+
+import com.cheezycode.notesample.models.UserRequest
+import com.cheezycode.notesample.models.UserResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface UserApi {
+    @POST("/users/signup")
+    suspend fun signup(@Body userRequest: UserRequest): Response<UserResponse>
+
+    @POST("/users/signin")
+    suspend fun signin(@Body userRequest: UserRequest): Response<UserResponse>
+}
